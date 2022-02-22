1230ca1b17059d77e21fcd2eee2345cb8b830f67
GLis4 <elisg1711@gmail.com>
2022-02-21 21:37:22 -0300

    started CRUD for task

--- ToDo-list/src/ManipularTask.java
+++ ToDo-list/src/ManipularTask.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class ManipularTask {
 	
-	List<Task> tskList = new ArrayList<>();
+	static List<Task> tskList = new ArrayList<>();
 	
 	
 	public static void main(String[] args) {
@@ -14,8 +14,38 @@ public class ManipularTask {
 		tsk.status = 1;
 		tsk.priority = "low";	
 		
-		//System.out.println(tsk.calculateTaskAge()+ "\n" + tsk.description );
 		System.out.println(Util.formatarTarefa(tsk));
 		
 	}
+	
+	public static void addTask(String description, byte status, String priority) {
+		
+		Task tsk = new Task();
+		tsk.description = description;
+		tsk.status = status;
+		tsk.priority = priority;
+		
+		tskList.add(tsk);
+	}
+	
+	public static void alterTask( int option, int idxAlter, String propertyToAlter) {
+		
+		switch(option) {
+		case 1 :
+			//description
+			tskList.get(idxAlter).description = propertyToAlter;
+			break;
+		case 2 : 
+			//priority
+			tskList.get(idxAlter).priority = propertyToAlter;
+			break;
+		}
+	}
+	
+	public static void remTask(int idxRem) {
+		tskList.remove(idxRem);
+		
+	}
+	
+	
 }
